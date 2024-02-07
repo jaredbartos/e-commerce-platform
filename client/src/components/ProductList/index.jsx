17fd911cdbe5e06c5654f4dbd3b7cae8760f9e38
Jared Bartos <139281328+jaredbartos@users.noreply.github.com>
2024-02-07 14:02:13 -0600

    Refactor ProductList component

--- client/src/components/ProductList/index.jsx
+++ client/src/components/ProductList/index.jsx
@@ -1,44 +1,40 @@
 import { useEffect } from 'react';
 import ProductItem from '../ProductItem';
-import { useStoreContext } from '../../utils/GlobalState';
-import { UPDATE_PRODUCTS } from '../../utils/actions';
+import { useSelector, useDispatch } from 'react-redux';
+import { updateProducts } from '../../utils/globalState/slices/productsAndCartSlice';
+import { selectCurrentCategory } from '../../utils/globalState/slices/categoriesSlice';
+import { selectProducts } from '../../utils/globalState/slices/productsAndCartSlice';
 import { useQuery } from '@apollo/client';
 import { QUERY_PRODUCTS } from '../../utils/queries';
 import { idbPromise } from '../../utils/helpers';
 import spinner from '../../assets/spinner.gif';
 
 function ProductList() {
-  const [state, dispatch] = useStoreContext();
-
-  const { currentCategory } = state;
+  const dispatch = useDispatch();
+  const currentCategory = useSelector(selectCurrentCategory);
+  const products = useSelector(selectProducts);
 
   const { loading, data } = useQuery(QUERY_PRODUCTS);
 
   useEffect(() => {
     if (data) {
-      dispatch({
-        type: UPDATE_PRODUCTS,
-        products: data.products,
-      });
+      dispatch(updateProducts({ products: data.products }));
       data.products.forEach((product) => {
         idbPromise('products', 'put', product);
       });
     } else if (!loading) {
       idbPromise('products', 'get').then((products) => {
-        dispatch({
-          type: UPDATE_PRODUCTS,
-          products: products,
-        });
+        dispatch(updateProducts({ products: products }));
       });
     }
   }, [data, loading, dispatch]);
 
   function filterProducts() {
     if (!currentCategory) {
-      return state.products;
+      return products;
     }
 
-    return state.products.filter(
+    return products.filter(
       (product) => product.category._id === currentCategory
     );
   }
@@ -46,7 +42,7 @@ function ProductList() {
   return (
     <div className="my-2">
       <h2>Our Products:</h2>
-      {state.products.length ? (
+      {products.length ? (
         <div className="flex-row">
           {filterProducts().map((product) => (
             <ProductItem
