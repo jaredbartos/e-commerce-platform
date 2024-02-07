e7428a6d742c3efae0eacf41356f1f12de3fb9a3
Jared Bartos <139281328+jaredbartos@users.noreply.github.com>
2024-02-07 13:20:19 -0600

    Refactor CartItem component

--- client/src/components/CartItem/index.jsx
+++ client/src/components/CartItem/index.jsx
@@ -1,35 +1,27 @@
-import { useStoreContext } from "../../utils/GlobalState";
-import { REMOVE_FROM_CART, UPDATE_CART_QUANTITY } from "../../utils/actions";
+import { useDispatch } from "react-redux";
+import { removeFromCart, updateCartQuantity } from "../../utils/globalState/slices/productsAndCartSlice";
 import { idbPromise } from "../../utils/helpers";
 
 const CartItem = ({ item }) => {
 
-  const [, dispatch] = useStoreContext();
+  const dispatch = useDispatch();
 
-  const removeFromCart = item => {
-    dispatch({
-      type: REMOVE_FROM_CART,
-      _id: item._id
-    });
+  const removeFromCartHandler = item => {
+    dispatch(removeFromCart({ _id: item._id }));
     idbPromise('cart', 'delete', { ...item });
-
   };
 
   const onChange = (e) => {
     const value = e.target.value;
     if (value === '0') {
-      dispatch({
-        type: REMOVE_FROM_CART,
-        _id: item._id
-      });
+      dispatch(removeFromCart({ _id: item._id }));
       idbPromise('cart', 'delete', { ...item });
 
     } else {
-      dispatch({
-        type: UPDATE_CART_QUANTITY,
+      dispatch(updateCartQuantity({
         _id: item._id,
         purchaseQuantity: parseInt(value)
-      });
+      }));
       idbPromise('cart', 'put', { ...item, purchaseQuantity: parseInt(value) });
 
     }
@@ -56,7 +48,7 @@ const CartItem = ({ item }) => {
           <span
             role="img"
             aria-label="trash"
-            onClick={() => removeFromCart(item)}
+            onClick={() => removeFromCartHandler(item)}
           >
             🗑️
           </span>
